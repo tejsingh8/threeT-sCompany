5a5dad76901a7487f5ba405c8616df4501e5ade0
tejsingh8 <52592952+tejsingh8@users.noreply.github.com>
2021-02-25 14:27:31 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,92 @@
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var slideIndex = 0;
+
+//jQuery to navigate to filter subsections function
+$().ready(function() {
+
+    var selected_filter = urlParams.get('filter');
+
+    if (selected_filter == null) {
+        selected_filter = "all"
+    }
+    console.log(selected_filter)
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    console.log(x)
+
+    if (selected_filter == "all") selected_filter = "";
+    for (i = 0; i < x.length; i++) {
+
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(selected_filter) > -1) {
+            w3AddClass(x[i], "show");
+        }
+    }
+});
+
+showSlides();
+
+//function used to filter the selection for the subsections
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+//function used to show the selection filter option
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+}
+
+//function used to hide the selection filter option
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " .active";
+    });
+}
+
+//This is the home page slide show javascript
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
